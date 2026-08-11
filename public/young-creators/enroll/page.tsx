7baab9f96d351f394ab1bf2354d67e856c7ad448
Andre Logan <andrelogan@Andres-MacBook-Air-2.local>
2026-08-11 18:14:54 -0400

    fixed enroll/page.tsx

--- public/young-creators/enroll/page.tsx
+++ public/young-creators/enroll/page.tsx
@@ -1,32 +1,11 @@
-import RegistrationHero from "@/components/young-creators/RegistrationHero";
-import ParentInfo from "@/components/young-creators/ParentInfo";
-import StudentInfo from "@/components/young-creators/StudentInfo";
-import TechnologyCheck from "@/components/young-creators/TechnologyCheck";
-import TuitionSelection from "@/components/young-creators/TuitionSelection";
-import ParentNotes from "@/components/young-creators/ParentNotes";
-import Agreement from "@/components/young-creators/Agreements";
-import SubmitRegistration from "@/components/young-creators/SubmitRegistration";
+import RegistrationForm from "@/components/young-creators/RegistrationForm";
 
 export default function EnrollPage() {
   return (
-    <main className="bg-black text-white">
-
-      <RegistrationHero />
-
-      <ParentInfo />
-
-      <StudentInfo />
-
-      <TechnologyCheck />
-
-      <TuitionSelection />
-
-      <ParentNotes />
-
-      <Agreement />
-
-      <SubmitRegistration />
-
+    <main className="bg-slate-100 py-16">
+      <div className="mx-auto max-w-6xl px-6">
+        <RegistrationForm />
+      </div>
     </main>
   );
 }
